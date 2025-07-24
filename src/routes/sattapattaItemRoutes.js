67e4c669feacfaf3b0ee4a76ee27bdd556db0fcd
mebharat73 <mebharat73@gmail.com>
2025-07-24 16:28:34 +0545

    hhhhhhh

--- src/routes/sattapattaItemRoutes.js
+++ src/routes/sattapattaItemRoutes.js
@@ -15,7 +15,7 @@ router.put('/edit/:id', auth, upload.array('imageFiles', 5), sattapattaItemContr
 router.get('/', sattapattaItemController.getAllItems); // No auth needed for getting all items (public)
 router.get('/my-items', auth, sattapattaItemController.getItemsByOwner); // Apply auth middleware
 router.get('/:id', sattapattaItemController.getItemById); // No auth needed for getting a single item (public)
-router.put('/:id', auth, sattapattaItemController.updateItem); // Apply auth middleware
+// router.put('/:id', auth, sattapattaItemController.updateItem); // Apply auth middleware
 router.delete('/:id', auth, sattapattaItemController.deleteItem); // Apply auth middleware
 
 export default router;
